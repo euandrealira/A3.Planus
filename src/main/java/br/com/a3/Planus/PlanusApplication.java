487f2d29680696adc30397fe4d3cee364b0eda23
Felipe Kovacs <felipe_kovacs@hotmail.com>
2025-09-21 15:52:33 -0300

    commit initial

---
+++ src/main/java/br/com/a3/Planus/PlanusApplication.java
@@ -0,0 +1,13 @@
+package br.com.a3.Planus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanusApplication.class, args);
+	}
+
+}
